12138074c67e0b955d5cba037e3e406198af02a1
Hanafujifilm <tuandatnguyen2905@gmail.com>
2026-08-25 23:51:42 +0700

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "./firebase";
+import App from "./App";
+import "./styles.css";
+
+function Root() {
+  const [user, setUser] = React.useState(undefined);
+
+  React.useEffect(() => {
+    return onAuthStateChanged(auth, setUser);
+  }, []);
+
+  if (user === undefined) {
+    return <div className="loading">Đang mở quỹ của chúng mình… 💕</div>;
+  }
+
+  return <App user={user} />;
+}
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode><Root /></React.StrictMode>
+);
